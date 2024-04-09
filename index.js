6c959a985271ed53fadc514739c5f8cbdb23ade1
Calvin Chege <calvinchege1@gmail.com>
2024-04-09 18:20:25 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching (names, string) {
+    return names.filter((driversMatch) => 
+        driversMatch.toLowerCase() === string.toLowerCase()
+    );
+}
+
+function fuzzyMatch (names, string) {
+    return names.filter((driversMatch) => 
+        driversMatch.toLowerCase().indexOf(string.toLowerCase()) === 0);
+}
+
+function matchName (driverArray, string) {
+    return driverArray.filter((record) => record.name === string);
+}
